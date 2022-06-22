e5690c9e8c473d397a0855029c69e1d7fd7bb3d1
doanandreas <intern.doan@sayurbox.com>
2022-06-22 21:17:34 +0700

    add env variable for peer id

--- puppeteer.js
+++ puppeteer.js
@@ -1,15 +1,19 @@
 const path = require("path");
 const puppeteer = require("puppeteer");
 
+require("dotenv").config();
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
+  page.on("console", (msg) => console.log("LOG:", msg.text()));
 
   await page.goto(`file://${path.join(__dirname, "dist", "index.html")}`, {
     waitUntil: "networkidle0",
   });
   await page.screenshot({ path: path.join(__dirname, "img", "test.png") });
-  await page.evaluate(() => connectToRoom("A"));
+  await page.evaluate(() => connectToRoom(process.env.PEER_ID));
 
-  await browser.close();
+  // await browser.close();
 })();

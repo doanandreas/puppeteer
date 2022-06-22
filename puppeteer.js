c8206a533c8c1f7c607a6b9fcb44e05febbf4c77
doanandreas <intern.doan@sayurbox.com>
2022-06-22 21:19:45 +0700

    add no sandbox args

--- puppeteer.js
+++ puppeteer.js
@@ -4,7 +4,10 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/google-chrome",
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
 
   page.on("console", (msg) => console.log("LOG:", msg.text()));

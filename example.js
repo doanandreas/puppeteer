a6aa0fae5338975c2d4433b9e944f694385c33f2
doanandreas <intern.doan@sayurbox.com>
2022-06-22 14:59:28 +0700

    add no sandbox args

--- example.js
+++ example.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/google-chrome",
+    args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
   await page.goto("http://google.com/");

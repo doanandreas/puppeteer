8a6bfa43cb58c2e9446acd161b0b0f3a1b649b95
doanandreas <intern.doan@sayurbox.com>
2022-06-22 14:55:03 +0700

    install deps for chrome

--- example.js
+++ example.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/google-chrome",
+  });
   const page = await browser.newPage();
   await page.goto("http://google.com/");
   await page.screenshot({ path: "./img/google.png" });

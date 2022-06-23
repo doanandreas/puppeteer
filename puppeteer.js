136443d7f2c8874ea683adb1c592c8cde11ab1a7
doanandreas <intern.doan@sayurbox.com>
2022-06-23 13:05:37 +0700

    add testing logic

--- puppeteer.js
+++ puppeteer.js
@@ -3,21 +3,34 @@ const puppeteer = require("puppeteer");
 
 require("dotenv").config();
 
+const { PEER_ID, TRIAL_NUMBER, PEER_NUMBER } = process.env;
+
 (async () => {
-  const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/google-chrome",
-    args: ["--no-sandbox"],
-  });
+  // const browser = await puppeteer.launch({
+  //   executablePath: "/usr/bin/google-chrome",
+  //   args: ["--no-sandbox"],
+  // });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const PEER_ID = process.env.PEER_ID;
 
   page.on("console", (msg) => console.log("LOG:", msg.text()));
 
-  await page.goto(`file://${path.join(__dirname, "dist", "index.html")}`, {
-    waitUntil: "networkidle0",
-  });
+  // await page.goto(`file://${path.join(__dirname, "dist", "index.html")}`, {
+  //   waitUntil: "networkidle0",
+  // });
+  await page.goto(
+    "file:///C:/dev-ssd/%23p2p-collab/puppeteer/dist/index.html",
+    {
+      waitUntil: "networkidle0",
+    }
+  );
   await page.screenshot({ path: path.join(__dirname, "img", "test.png") });
-  await page.evaluate((id) => connectToRoom(id), PEER_ID);
+  await page.evaluate(
+    (id, trials, peers) => connectToRoom(id, trials, peers),
+    PEER_ID,
+    Number(TRIAL_NUMBER),
+    Number(PEER_NUMBER)
+  );
 
   // await browser.close();
 })();
--- src/index.js
+++ src/index.js
@@ -1,22 +1,69 @@
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
 
-const connectToRoom = (id) => {
+let readyPeer = 0;
+let currTrial = 0;
+let rttArray = [];
+
+const connectToRoom = (id, trialNumber, peerNumber) => {
   const doc = new Y.Doc();
-  const map = doc.getMap();
+  const statusMap = doc.getMap("status");
+  const requestMap = doc.getMap("request");
+  const responseMap = doc.getMap("response");
 
   const roomId = "tldraw-p2p-eval";
   const provider = new WebrtcProvider(roomId, doc);
 
-  map.observe((ymapEvent) => {
+  statusMap.observe((ymapEvent) => {
+    ymapEvent.changes.keys.forEach((change, key) => {
+      if (change.action === "add" && statusMap.get(key) === "READY") {
+        readyPeer++;
+        console.log(
+          `Peer ${key} is READY for testing. Number of peers: ${readyPeer}`
+        );
+
+        if (readyPeer === peerNumber) {
+          console.log("All peers are READY.");
+
+          doc.transact(() => {
+            requestMap.set(id, new Date().getTime());
+          }, id);
+        }
+      }
+    });
+  });
+
+  requestMap.observe((ymapEvent) => {
+    ymapEvent.changes.keys.forEach((change, key) => {
+      if (change.action === "add" || change.action === "update") {
+        console.log(
+          `Peer "${key}" request timestamp: "${requestMap.get(key)}".`
+        );
+
+        if (key !== id) {
+          doc.transact(() => {
+            responseMap.set(`${id}:${key}`, new Date().getTime());
+          }, id);
+        }
+      }
+    });
+  });
+
+  // responseMap.observe((ymapEvent) => {
+  //   ymapEvent.changes.keys.forEach((change, key) => {
+
+  //   });
+  // });
+
+  responseMap.observe((ymapEvent) => {
     ymapEvent.changes.keys.forEach((change, key) => {
       if (change.action === "add") {
         console.log(
-          `Property "${key}" was added. Initial value: "${map.get(key)}".`
+          `Property "${key}" was added. Current time: "${new Date().getTime()}".`
         );
       } else if (change.action === "update") {
         console.log(
-          `Property "${key}" was updated. New value: "${map.get(
+          `Property "${key}" was updated. New value: "${responseMap.get(
             key
           )}". Previous value: "${change.oldValue}".`
         );
@@ -29,7 +76,7 @@ const connectToRoom = (id) => {
   });
 
   doc.transact(() => {
-    map.set(id, new Date().getTime());
+    statusMap.set(id, "READY");
   }, id);
 };
 

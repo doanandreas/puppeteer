3a90840d50a231a71b879567cf5a11485b0d7730
doanandreas <intern.doan@sayurbox.com>
2022-06-23 13:06:21 +0700

    uncomment command for vm

--- puppeteer.js
+++ puppeteer.js
@@ -6,24 +6,24 @@ require("dotenv").config();
 const { PEER_ID, TRIAL_NUMBER, PEER_NUMBER } = process.env;
 
 (async () => {
-  // const browser = await puppeteer.launch({
-  //   executablePath: "/usr/bin/google-chrome",
-  //   args: ["--no-sandbox"],
-  // });
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/google-chrome",
+    args: ["--no-sandbox"],
+  });
+  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   page.on("console", (msg) => console.log("LOG:", msg.text()));
 
-  // await page.goto(`file://${path.join(__dirname, "dist", "index.html")}`, {
-  //   waitUntil: "networkidle0",
-  // });
-  await page.goto(
-    "file:///C:/dev-ssd/%23p2p-collab/puppeteer/dist/index.html",
-    {
-      waitUntil: "networkidle0",
-    }
-  );
+  await page.goto(`file://${path.join(__dirname, "dist", "index.html")}`, {
+    waitUntil: "networkidle0",
+  });
+  // await page.goto(
+  //   "file:///C:/dev-ssd/%23p2p-collab/puppeteer/dist/index.html",
+  //   {
+  //     waitUntil: "networkidle0",
+  //   }
+  // );
   await page.screenshot({ path: path.join(__dirname, "img", "test.png") });
   await page.evaluate(
     (id, trials, peers) => connectToRoom(id, trials, peers),
